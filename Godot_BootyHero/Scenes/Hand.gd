4b4e76604cee987235744024db02f86771991be6
Monika <monika.galkewitsch@hs-furtwangen.de>
2024-01-28 15:28:54 +0100

    Booty Slap WIP

---
+++ Godot_BootyHero/Scenes/Hand.gd
@@ -0,0 +1,9 @@
+extends Sprite3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("looking for Booty")
+	pass
+	
+func _body_entered(event):
+	print("Booty found")
