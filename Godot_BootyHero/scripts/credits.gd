23b053d3fe5fb52bc0149627e5d04709a14e149c
Plagiatus <lukas.scheuerle@web.de>
2024-01-29 20:21:38 +0100

    you can leave the credits again

--- Godot_BootyHero/scripts/credits.gd
+++ Godot_BootyHero/scripts/credits.gd
@@ -1,16 +1,9 @@
 extends Node2D
 
-func _init():
-	print("test instance")
-	
-func loadStuff():
-	print("testStuff")
+@onready var titlescreen = load("res://Scenes/titel_screen.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_input_manager_input_received(type):
+	if(type == "lr"):
+		var newTitlescreen = titlescreen.instantiate()
+		get_tree().root.add_child(newTitlescreen)
+		queue_free()
